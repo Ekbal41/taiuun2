07716f4ece91b4527a474a1024fb6da46ae128ca
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-08-15 00:07:00 +0600

    ok

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -5,18 +5,15 @@ import ActionCard from "@/components/home/ActionCard";
 import Category from "@/components/home/Category";
 import Hero from "@/components/home/Hero";
 import OurStory from "@/components/home/OurStory";
-import { Box } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
-      <Box>
-        <Hero />
-        <OurStory />
-        <Category />
-        <ActionCard />
-        <Action />
-      </Box>
+      <Hero />
+      <OurStory />
+      <Category />
+      <ActionCard />
+      <Action />
     </>
   );
 }
